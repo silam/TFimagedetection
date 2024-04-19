7351af7892b85e273bfc505766db8410264a4c4f
Si Lam <silam@hotmail.com>
2024-04-18 22:13:33 -0500

    Webcam prediction

---
+++ webcam.js
@@ -0,0 +1,28 @@
+import "@tensorflow/tfjs"
+import * as cocoSsd from "@tensorflow-models/coco-ssd"
+import { takePicture, startWebcam, handleFilePicker, showResult } from "./utils";
+
+
+const webcamButton = document.getElementById("webcam");
+const captureButton = document.getElementById("pause");
+const video = document.querySelector("video");
+
+let model;
+
+const init = async () => {
+    model = await cocoSsd.load();
+}
+
+webcamButton.onclick = () => {
+    startWebcam(video)
+}
+
+captureButton.onclick = () => takePicture(video, predict);
+
+const predict = async (img) => {
+    console.log("Detecting image");
+    const predictions = await model.detect(img);
+    console.log(predictions);
+    showResult(predictions)
+}
+init();

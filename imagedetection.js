c64eef400cfab1625497e23baee32f055a47c03e
Si Lam <silam@hotmail.com>
2024-04-18 21:24:01 -0500

    Init commit

---
+++ imagedetection.js
@@ -0,0 +1,23 @@
+import "@tensorflow/tfjs"
+import * as cocoSsd from "@tensorflow-models/coco-ssd"
+import { handleFilePicker, showResult } from "./utils";
+
+let model;
+
+const init = async () => {
+    model = await cocoSsd.load()
+
+    handleFilePicker(predict);
+
+
+}
+
+const predict = async (img) => {
+    console.log("Detecting image");
+    const predictions = await model.detect(img);
+    console.log(predictions);
+    showResult(predictions)
+}
+
+
+init();
